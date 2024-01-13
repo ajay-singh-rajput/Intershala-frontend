de360c7bed5e5dfaada64e850cebf153c36ac8f1
ajay-singh-rajput <ajay091.ar@gmail.com>
2024-01-13 13:56:07 +0530

    api linked

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import Nav from './component/Nav'
 import { Route, Routes } from 'react-router-dom'
 import Sinup from './component/Sinup'
+import axios from 'axios'
 
 
 const App = () => {
+
+  const connectServer = async ()=>{
+    try {
+      const {data} = await axios.get("http://localhost:8080/")
+    console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
   return (
     <>
     <div>
       <Nav/>
+      <button onClick={connectServer}>connect</button>
 
       <div>
         <Routes>
